4d262ecadfb339962383948fcd309006638079ff
yadena <tekouakarele@gmail.com>
2023-06-29 17:16:42 +0100

    initial commmit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'username' => 'admin',
+            'firstname' => 'Admin',
+            'lastname' => 'Admin',
+            'email' => 'admin@argon.com',
+            'password' => bcrypt('secret')
+        ]);
+    }
+}
