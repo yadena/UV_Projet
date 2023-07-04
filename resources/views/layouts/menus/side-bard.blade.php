7b9de572fc9db334e6b5ce899f5cd36b9321031e
Meli Baudouin <baudouin.meli@kimbocare.com>
2023-07-04 17:16:33 +0100

    Some layout fixs

--- resources/views/layouts/menus/side-bard.blade.php
+++ resources/views/layouts/menus/side-bard.blade.php
@@ -16,7 +16,7 @@
                 <a class="nav-link active" href="./pages/dashboard.html">
                     <div
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
+                        <i class="ni ni-ambulance text-primary text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Dashboard</span>
                 </a>
